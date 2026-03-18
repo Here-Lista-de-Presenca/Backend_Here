677953104a1117d428980e1c43234104cbdcc59b
murieldemonti <muriel.demonti@outlook.com>
2026-03-17 21:04:29 -0300

    Feat: Criação do repositório registro de presença com JpaRepository

---
+++ src/main/java/com/unisul/here/dao/RegistroPresencaDAO.java
@@ -0,0 +1,7 @@
+package com.unisul.here.dao;
+
+import com.unisul.here.model.RegistroPresenca;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegistroPresencaDAO extends JpaRepository<RegistroPresenca, Integer> {
+}
