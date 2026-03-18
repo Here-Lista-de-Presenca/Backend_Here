83f547f2c56cbac7b09bde4d8ae11e7b5a62e154
Adhan Oda <169103008+AdhanOda@users.noreply.github.com>
2026-03-17 22:51:52 -0300

    Feat: criado classe RegistroPresenca

---
+++ src/main/java/com/unisul/here/controller/RegistroPresencaController.java
@@ -0,0 +1,22 @@
+package com.unisul.here.controller;
+
+import com.unisul.here.model.RegistroPresenca;
+import com.unisul.here.service.RegistroPresencaService;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/presenca")
+public class RegistroPresencaController {
+
+    private final RegistroPresencaService service;
+
+    public RegistroPresencaController(RegistroPresencaService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/registrar")
+    public RegistroPresenca registrarPresenca() {
+        return service.registrarPresenca();
+    }
+}
