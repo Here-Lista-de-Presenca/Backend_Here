39c98a7b9e09dff57ef08e9e420affde6306ad95
Adhan Oda <169103008+AdhanOda@users.noreply.github.com>
2026-03-17 22:52:36 -0300

    refactor: removido classe RegistroPresencaDAO

--- src/main/java/com/unisul/here/dao/RegistroPresencaDAO.java
+++
@@ -1,7 +0,0 @@
-package com.unisul.here.dao;
-
-import com.unisul.here.model.RegistroPresenca;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegistroPresencaDAO extends JpaRepository<RegistroPresenca, Integer> {
-}
