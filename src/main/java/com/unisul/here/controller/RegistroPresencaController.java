3218defd980e155a46e9afd99de574ae14c48692
mauricio.gabriel <mauriciobatistagabriell@gmail.com>
2026-04-07 23:25:22 -0300

    feat:Add validação localização registro de presença

--- src/main/java/com/unisul/here/controller/RegistroPresencaController.java
+++ src/main/java/com/unisul/here/controller/RegistroPresencaController.java
@@ -1,5 +1,6 @@
 package com.unisul.here.controller;
 
+import com.unisul.here.dto.LocalizacaoDTO;
 import com.unisul.here.model.RegistroPresenca;
 import com.unisul.here.service.RegistroPresencaService;
 import org.springframework.http.ResponseEntity;
@@ -19,8 +20,14 @@ public class RegistroPresencaController {
     }
 
     @PostMapping("/registrar/{id}")
-    public ResponseEntity<?> registrar(@PathVariable Long id) {
-        RegistroPresenca registro = service.registrarPresenca(id, LocalDateTime.now());
+    public ResponseEntity<?> registrar(
+            @PathVariable Long id,
+            @RequestBody LocalizacaoDTO loc,
+            @RequestParam(required = false) LocalDateTime momento) {
+
+        LocalDateTime dataParaRegistro = (momento != null) ? momento : LocalDateTime.now();
+
+        RegistroPresenca registro = service.registrarPresenca(id, dataParaRegistro, loc.latUser(), loc.lonUser());
         return ResponseEntity.ok(registro);
     }
 }
--- src/main/java/com/unisul/here/service/RegistroPresencaService.java
+++ src/main/java/com/unisul/here/service/RegistroPresencaService.java
@@ -15,13 +15,33 @@ public class RegistroPresencaService {
     private final RegistroPresencaRepository presencaRepository;
     private final CadastroUsuarioRepository usuarioRepository;
 
+    // Coordenadas da R. Antônio Dib Mussi, 366
+    private static final double LAT_ALVO = -27.5935;
+    private static final double LON_ALVO = -48.5528;
+    private static final double TOLERANCIA_DISTANCIA = 100.0;
+
     public RegistroPresencaService(RegistroPresencaRepository presencaRepository,
                                    CadastroUsuarioRepository usuarioRepository) {
         this.presencaRepository = presencaRepository;
         this.usuarioRepository = usuarioRepository;
     }
 
-    public RegistroPresenca registrarPresenca(Long usuarioId, LocalDateTime dataHoraRegistro) {
+    public RegistroPresenca registrarPresenca(Long usuarioId, LocalDateTime dataHoraRegistro, double latUser, double lonUser) {
+        ValidarHoraRegistro(dataHoraRegistro);
+
+        ValidarLocalizacaoUsuario(latUser, lonUser);
+
+        CadastroUsuario usuario = usuarioRepository.findById(usuarioId)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+
+        RegistroPresenca registro = new RegistroPresenca();
+        registro.setHoraRegistro(dataHoraRegistro);
+        registro.setUsuario(usuario);
+
+        return presencaRepository.save(registro);
+    }
+
+    private void ValidarHoraRegistro(LocalDateTime dataHoraRegistro){
         LocalTime agora = dataHoraRegistro.toLocalTime();
 
         LocalTime inicioPermitido = LocalTime.of(19, 50);
@@ -34,14 +54,29 @@ public class RegistroPresencaService {
         if (agora.isAfter(limiteRegistro)) {
             throw new RuntimeException("Tempo de registro encerrado.");
         }
+    }
 
-        CadastroUsuario usuario = usuarioRepository.findById(usuarioId)
-                .orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+    private void ValidarLocalizacaoUsuario(double latUser, double lonUser){
+        double distancia = CalcularDistancia(latUser, lonUser);
 
-        RegistroPresenca registro = new RegistroPresenca();
-        registro.setHoraRegistro(dataHoraRegistro);
-        registro.setUsuario(usuario);
+        if (distancia > TOLERANCIA_DISTANCIA) {
+            throw new RuntimeException("Usuário fora do local permitido para registro.");
+        }
+    }
 
-        return presencaRepository.save(registro);
+    private double CalcularDistancia(double lat1, double lon1) {
+        double raioTerra = 6371e3;
+        double phi1 = Math.toRadians(lat1);
+        double phi2 = Math.toRadians(RegistroPresencaService.LAT_ALVO);
+        double deltaPhi = Math.toRadians(RegistroPresencaService.LAT_ALVO - lat1);
+        double deltaLambda = Math.toRadians(RegistroPresencaService.LON_ALVO - lon1);
+
+        double a = Math.sin(deltaPhi / 2) * Math.sin(deltaPhi / 2) +
+                Math.cos(phi1) * Math.cos(phi2) *
+                        Math.sin(deltaLambda / 2) * Math.sin(deltaLambda / 2);
+
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return raioTerra * c;
     }
 }
